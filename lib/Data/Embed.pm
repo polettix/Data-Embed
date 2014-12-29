a25c4d5ce9e7fe590a322535b83c1a067b302076
Flavio Poletti <flavio@polettix.it>
2014-12-29 18:58:39 +0100

    Advanced and somehow finalized implementation
    ... on with the tests

--- lib/Data/Embed.pm
+++ lib/Data/Embed.pm
@@ -5,6 +5,114 @@ package Data::Embed;
 use strict;
 use warnings;
 use English qw< -no_match_vars >;
+use Exporter qw< import >;
+use Log::Log4perl::Tiny qw< :easy :dead_if_first >;
+
+our @EXPORT_OK = qw< writer reader embed embedded >;
+our @EXPORT = ();
+our %EXPORT_TAGS = (
+   all => \@EXPORT_OK,
+   reading => [ qw< reader embedded > ],
+   writing => [ qw< writer embed    > ],
+);
+
+=head1 FUNCTIONS
+
+=head2 B<< embed >>
+
+Embed new data inside a container file. The calling syntax is
+as follows:
+
+   embed($container, $hashref); # OR
+   embed($container, %keyvalue_pairs);
+
+The C<$container> parameter is the target file where the new data
+will be inserted.
+
+Additional parameters can be passed as key-value pairs either
+directly or through a hash reference. The following keys are
+supported:
+
+=over
+
+=item C<name>
+
+the name to associate to the section, optionally. If missing it will
+be set to the empty string
+
+=item C<fh>
+
+the filehandle from where data should be taken. The filehandle will be
+exausted starting from its current position
+
+=item C<filename>
+
+a filename or a reference to a scalar where data will be read from
+
+=item c<data>
+
+a scalar from where data will be read. If you have a huge amount of
+data, it's better to use the C<filename> key above passing a reference
+to the scalar holding the data.
+
+=back
+
+Options C<fh>, C<filename> and C<data> are exclusive and will be considered
+in the order above (first come, first served).
+
+This function does not return anything.
+
+=head2 B<< embedded >>
+
+Get a list of the embedded files inside a target container. The calling
+syntax is as follows:
+
+   my $arrayref = embedded($container); # scalar context, OR
+   my @files    = embedded($container); # list context
+
+The only input parameter is the C<$container> to use as input. It can
+be either a real filename, or a filehandle.
+
+Depending on the context, a list will be returned (in list context) or
+an array reference holding the list.
+
+Whatever the context, each item in the list is a L<Data::Embed::File>
+object that you can use to access the embedded file data (most notably,
+you'll be probably using its C<contents> or C<fh> methods).
+
+=head2 B<< writer >>
+
+This is a convenience wrapper around the constructor for
+L<Data::Embed::Writer>.
+
+=head2 B<< reader >>
+
+This is a convenience wrapper around the constructor for
+L<Data::Embed::Reader>.
+
+=cut
+
+sub writer {
+   require Data::Embed::Writer;
+   return Data::Embed::Writer->new(@_);
+}
+
+sub reader {
+   require Data::Embed::Reader;
+   return Data::Embed::Reader->new(@_);
+}
+
+sub embed {
+   my $writer = writer(shift)
+      or LOGCROAK 'could not get the writer object';
+   return $writer->add(@_);
+}
+
+sub embedded {
+   my $reader = reader(shift)
+      or LOGCROAK 'could not get the writer object';
+   return $reader->files();
+}
 
 1;
 __END__
--- lib/Data/Embed/Util.pm
+++ lib/Data/Embed/Util.pm
@@ -10,17 +10,51 @@ use Exporter qw< import >;
 our @EXPORT_OK = qw< STARTER TERMINATOR escape unescape >;
 our @EXPORT = (); # export nothing by default
 our %EXPORT_TAGS = (
-   all => [qw< STARTER TERMINATOR escape unescape >],
+   all => \@EXPORT_OK,
    escaping => [qw< escape unescape >],
    constants => [qw< STARTER TERMINATOR >],
 );
 
 =head1 FUNCTIONS
 
-All these functions are 
+All these functions are not exported by default. You can use the
+following tags for importing them:
+
+=over
+
+=item C<< all >>
+
+all of them
+
+=item C<< escaping >>
+
+the escaping functions C<escape> and C<unescape>
+
+=item C<< constants >>
+
+the two constants C<STARTER> and C<TERMINATOR>
+
+=back
 
 =head2 B<< STARTER >>
 
+a string indicating the start of an index section
+
+=head2 B<< TERMINATOR >>
+
+a string indicating the end of an index section
+
+=head2 B<< escape >>
+
+escape an input string. All alphanumeric characters, plus the
+underscore C<_>, hyphen C<-> and full stop C<.> are preserved, while
+all the others are transformed into three-characters sequences C<%XX>
+where XX are two hexadecimal digits.
+
+=head2 B<< unescape >>
+
+unescape an input string with the reverse action performed by
+L</escape>.
 
 =cut
 
