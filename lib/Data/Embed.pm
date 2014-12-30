784e0261763ac0705d668f061b112d1f5b7e76a0
Flavio Poletti <flavio@polettix.it>
2014-12-30 17:23:58 +0100

    added SYNOPSYS and DESCRIPTION

--- lib/Data/Embed.pm
+++ lib/Data/Embed.pm
@@ -116,3 +116,84 @@ sub embedded {
 
 1;
 __END__
+
+=head1 SYNOPSYS
+
+   use Data::Embed qw< embed embedded >;
+
+   # this is the file where thing will be embedded, at the end
+   my $container = '/path/to/some/file';
+
+   # first of all we embed an external file
+   my $datafile  = '/path/to/data.tar.gz';
+   embed($container, name => 'data.tar.gz', filename => $datafile);
+
+   # we can also embed some data, directly
+   use Data::Dumper;
+   my $conf = { ... };
+   embed($container, name => 'config.yml', data => Dumper($conf));
+
+   # if the data is in a scalar but it's huge, use filename and
+   # pass a reference to the scalar so no copy will happen
+   my $huge_png = ...;
+   embed($container, name => 'image.png', filename => \$huge_png);
+
+   # to retrieve the stuff, use embedded()
+   my @files = embedded($container);
+
+   # each item in @files is a Data::Embed::File object
+
+   # get whole contents of file
+   my $config_text = $files[1]->contents();
+
+   # otherwise, you can get a filehandle and use it, e.g. to
+   # dump it on standard output
+   my $data_fh = $files[0]->fh();
+   binmode STDOUT;
+   print {*STDOUT} <$data_fh>;
+
+   # or save the file back, using the available name
+   open my $ofh, '>:raw', $file[2]->name(); # well, do your checks!
+   my $ifh = $files[2]->fh();
+   while (! eof $ifh) {
+      read $ifh, my $buffer, 4096
+         or last; # do proper checks in production!
+      print {$ofh} $buffer;
+   }
+
+
+=head1 DESCRIPTION
+
+This module allows you to manage embedding data at the end of other
+files, providing both means for embedding the data (L</embed> and
+L</writer>) and accessing them (L</embedded> and L</reader>).
+
+How can this be helpful? For example, suppose that you want to
+bring some data along with your perl script, some of which might
+be binary (e.g. an image, or a tar archive), you can embed these data
+inside the perl and then retrieve them. For example, this can be the
+basis for an installer.
+
+For embedding data, you can use the L</embed> function. See the relevant
+documentation or the examples in the L</SYNOPSYS> to use it properly.
+
+For extracting the embedded data, you can use the L</embedded> function
+and access each embedded file as a L<Data::Embed::File> object. You can
+then use its methods C<contents> for accessing the whole data, or get
+a filehandle through C<fh> and avoid getting the whole data in memory
+at once.
+
+Note: the filehandle provided by the C<fh> method of L<Data::Embed::File>
+is actually a L<IO::Slice> object, so it might not support all the
+functions/methods of a regular filehandle.
+
+You can also access the lower level interface through the two functions
+L</reader> and L</writer>. See the documentation
+for L<Data::Embed::Reader> and L<Data::Embed::Writer>.
+
+=head1 SEE ALSO
+
+L<Data::Section> covers a somehow similar need but differently. In
+particular, you should look at it if you want to be able to modify
+the data you want to embed directly, e.g. if you are embedding some
+textual templates that you want to tweak.
